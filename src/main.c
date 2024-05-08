0673f8b572b575bd5f3f9297cff615d61494dcdf
Dominik Kaiser <dkaiser@1-C-7.42heilbronn.de>
2024-05-08 16:46:09 +0200

    Add placeholder main

--- src/main.c
+++ src/main.c
@@ -6,8 +6,13 @@
 /*   By: dkaiser <dkaiser@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/08 14:14:13 by dkaiser           #+#    #+#             */
-/*   Updated: 2024/05/08 14:14:21 by dkaiser          ###   ########.fr       */
+/*   Updated: 2024/05/08 15:56:23 by dkaiser          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
+
+int main(void) {
+
+    return 0;
+}
