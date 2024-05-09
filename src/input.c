e206d293aa947dc56fa8408c42dccaff9cb423c3
Dominik Kaiser <dkaiser@1-C-7.42heilbronn.de>
2024-05-09 19:32:33 +0200

    Add arrow key movement

--- src/input.c
+++ src/input.c
@@ -6,7 +6,7 @@
 /*   By: dkaiser <dkaiser@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/09 15:37:40 by dkaiser           #+#    #+#             */
-/*   Updated: 2024/05/09 17:48:20 by dkaiser          ###   ########.fr       */
+/*   Updated: 2024/05/09 19:32:18 by dkaiser          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,26 @@
 
 int	on_key_down(int keycode, t_game *game)
 {
-	if (keycode == 13)
+	if (keycode == 13 || keycode == 126)
 		game->input_direction |= UP;
-	else if (keycode == 0)
+	else if (keycode == 0 || keycode == 123)
 		game->input_direction |= LEFT;
-	else if (keycode == 1)
+	else if (keycode == 1 || keycode == 125)
 		game->input_direction |= DOWN;
-	else if (keycode == 2)
+	else if (keycode == 2 || keycode == 124)
 		game->input_direction |= RIGHT;
 	return (0);
 }
 
 int	on_key_up(int keycode, t_game *game)
 {
-	if (keycode == 13)
+	if (keycode == 13 || keycode == 126)
 		game->input_direction &= ~UP;
-	else if (keycode == 0)
+	else if (keycode == 0 || keycode == 123)
 		game->input_direction &= ~LEFT;
-	else if (keycode == 1)
+	else if (keycode == 1 || keycode == 125)
 		game->input_direction &= ~DOWN;
-	else if (keycode == 2)
+	else if (keycode == 2 || keycode == 124)
 		game->input_direction &= ~RIGHT;
 	return (0);
 }
